c503d3bd0113532a6fc88d2a0368672406f1104f
Michał Popiel <131244219+Kogut01@users.noreply.github.com>
2026-09-11 15:25:51 +0200

    fix(k3s): separate prod and dev infra and some renames (#27)

--- cluster/delete.sh
+++ cluster/delete.sh
@@ -1,7 +1,28 @@
 #!/bin/bash
+set -euo pipefail
+
+ ENV="${1:-}"
+
+ if [[ -z "$ENV" ]]; then
+    echo "❌ Use: ./apply.sh <dev|prod>"
+    exit 1
+fi
+
+if [[ ! -d "../overlays/${ENV}" ]]; then
+    echo "❌ Directory ../overlays/${ENV} does not exist!"
+    exit 1
+fi
+
+if [[ "$ENV" == "prod" ]]; then
+   read -rp "⚠️ Delete on PRODUCTION. Type 'yes' to confirm: " c
+   [[ "$c" == "yes" ]] || { echo "Cancel."; exit 1; }
+fi
+
 echo "🔥 Start clearing the cluster..."
 echo ""
-sudo KUBECONFIG=/etc/rancher/k3s/k3s.yaml kapp delete -a antiginx-cluster -y
-sudo kubectl delete namespace monitoring antiginx data test-app --ignore-not-found
+
+sudo KUBECONFIG=/etc/rancher/k3s/k3s.yaml kapp delete -a "antiginx-${ENV}" -y
+sudo kubectl delete namespace monitoring antiginx data scan-targets-dev --ignore-not-found
+
 echo ""
 echo "✅ Everything deleted! The cluster is completely clean."
